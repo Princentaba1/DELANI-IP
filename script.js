ed36f87f79cbacce1447a955adea2b5c8c66d8ba
princentaba4@gmail.com <princentaba4@gmail.com>
2022-03-15 15:22:22 +0300

    16th commit added submit function on js

--- script.js
+++ script.js
@@ -85,3 +85,15 @@ $(".random3").click(function(){
       $(this).css("opacity","1")
       $("#Hover_design8").hide();
     })
+    $('.submit').click(function () {
+        let Name = $('#mce-FNAME').val();
+        let Email = $('#mce-EMAIL').val();
+        let Message = $('#mce-MESSAGE');
+        // let key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please make sure you have filled in the form correctly!');
+        } else {
+            alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+        }
+  });
+});
