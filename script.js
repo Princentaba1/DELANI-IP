1edf750ea1c49f2fa55efa925ce6ff1ac74e542a
princentaba4@gmail.com <princentaba4@gmail.com>
2022-03-15 15:11:21 +0300

    13th commit added js content to link hover effrct

---
+++ script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    $(".hide-design-image").click(function(){
+     $(".random").show();
+     $(this).hide();
+    })
+  $(".random").click(function(){
+    $(".hide-design-image").show();
+    $(this).hide();
+  })
+  $(".hide-development-image").click(function(){
+    $(".random2").show();
+    $(this).hide();
+   })
+ $(".random2").click(function(){
+   $(".hide-development-image").show();
+   $(this).hide();
+ })
+ $(".hide-product-image").click(function(){
+  $(".random3").show();
+  $(this).hide();
+ })
+$(".random3").click(function(){
+ $(".hide-product-image").show();
+ $(this).hide();
+})
+  
+   
+
+  
+    //Insert white box upon hover
+    $("#work1").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design").hide();
+    })
+    $("#work2").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design2").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design2").hide();
+    })
+    $("#work3").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design3").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design3").hide();
+    })
+    $("#work4").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design4").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design4").hide();
+    })
+    $("#work5").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design5").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design5").hide();
+    })
+    $("#work6").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design6").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design6").hide();
+    })
+    $("#work7").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design7").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design7").hide();
+    })
+    $("#work8").hover(function(){
+      $(this).css("opacity","0.3");
+      $("#Hover_design8").show();
+    },function(){
+      $(this).css("opacity","1")
+      $("#Hover_design8").hide();
+    })
